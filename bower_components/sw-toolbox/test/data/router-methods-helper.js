fd9ea1fab81a2027f03fbc86f5b3db4c943bdb46
Chiara Chiappini <cchiappini@google.com>
2017-08-31 21:15:41 +0100

    Removes bower

--- bower_components/sw-toolbox/test/data/router-methods-helper.js
+++
@@ -1,9 +0,0 @@
-self.getMethodToTest = () => {
-  const methodRegex = /[\?&]method=(\w+)/;
-  const methodRegexResult = methodRegex.exec(location.search);
-  if (!methodRegexResult) {
-    throw new Error('Unable to get the router method.');
-  }
-
-  return methodRegexResult[1];
-};
