f656e51c50c8dcc7a3737e13c2a683eefd1c1e87
Sebastian Benz <sebastian.benz@gmail.com>
2017-10-27 11:42:46 -0700

    add a favorite button sample (#994)
    * add a favorite button sample
    
    * add second sample with counter
    
    * addressed comments
    
    * fix lint errors

---
+++ backend/favorite.go
@@ -0,0 +1,109 @@
+// Copyright Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package backend
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	AMP_FAVORITE_COOKIE       = "amp-favorite"
+	AMP_FAVORITE_COUNT_COOKIE = "amp-favorite-with-count"
+)
+
+func InitFavoriteSample() {
+	http.HandleFunc("/favorite", handleFavorite)
+	http.HandleFunc("/favorite-with-count", handleFavoriteWithCount)
+}
+
+func handleFavorite(w http.ResponseWriter, r *http.Request) {
+	EnableCors(w, r)
+	SetContentTypeJson(w)
+	SetMaxAge(w, 0)
+	if r.Method == "POST" {
+		setFavorite(w, r)
+	} else if r.Method == "GET" {
+		getFavorite(w, r)
+	}
+}
+
+func getFavorite(w http.ResponseWriter, r *http.Request) {
+	favorite := readFavoriteFromCookie(r, AMP_FAVORITE_COOKIE)
+	writeFavorite(w, favorite)
+}
+
+func setFavorite(w http.ResponseWriter, r *http.Request) {
+	favorite := !readFavoriteFromCookie(r, AMP_FAVORITE_COOKIE)
+	writeFavoriteCookie(w, r, AMP_FAVORITE_COOKIE, favorite)
+	writeFavorite(w, favorite)
+}
+
+func handleFavoriteWithCount(w http.ResponseWriter, r *http.Request) {
+	EnableCors(w, r)
+	SetContentTypeJson(w)
+	SetMaxAge(w, 0)
+	if r.Method == "POST" {
+		setFavoriteWithCount(w, r)
+	} else if r.Method == "GET" {
+		getFavoriteWithCount(w, r)
+	}
+}
+
+func getFavoriteWithCount(w http.ResponseWriter, r *http.Request) {
+	favorite := readFavoriteFromCookie(r, AMP_FAVORITE_COUNT_COOKIE)
+	writeFavoriteWithCount(w, favorite)
+}
+
+func setFavoriteWithCount(w http.ResponseWriter, r *http.Request) {
+	favorite := !readFavoriteFromCookie(r, AMP_FAVORITE_COUNT_COOKIE)
+	writeFavoriteCookie(w, r, AMP_FAVORITE_COUNT_COOKIE, favorite)
+	writeFavoriteWithCount(w, favorite)
+}
+
+func writeFavoriteWithCount(w http.ResponseWriter, favorite bool) {
+	response := fmt.Sprintf("{ \"value\": %t, \"count\": 123}", favorite)
+	w.Write([]byte(response))
+}
+
+func writeFavorite(w http.ResponseWriter, favorite bool) {
+	response := fmt.Sprintf("%t", favorite)
+	w.Write([]byte(response))
+}
+
+func writeFavoriteCookie(w http.ResponseWriter, r *http.Request, name string, value bool) {
+	expireInOneDay := time.Now().AddDate(0, 0, 1)
+	cookie := &http.Cookie{
+		Name:    name,
+		Expires: expireInOneDay,
+		Path:    "/",
+		Value:   strconv.FormatBool(value),
+	}
+	http.SetCookie(w, cookie)
+}
+
+func readFavoriteFromCookie(r *http.Request, name string) bool {
+	cookie, err := r.Cookie(name)
+	if err != nil {
+		return false
+	}
+	favorite, err := strconv.ParseBool(cookie.Value)
+	if err != nil {
+		return false
+	}
+	return favorite
+}
