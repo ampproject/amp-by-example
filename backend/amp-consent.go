717389ccb1cf4955d303d7313ba07a31740f10d1
Martin Schierle <martin.schierle@gmail.com>
2018-04-11 16:10:14 +0200

    Initial draft of changes to show example flows for amp-consent (#1212)
    * Initial draft of changes to show example flows for amp-consent
    
    * Minor improvements (css classnames more speaking etc.)

---
+++ backend/amp-consent.go
@@ -0,0 +1,42 @@
+// Copyright Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package backend
+
+import (
+	"net/http"
+)
+
+const (
+	CONSENT_SAMPLE_PATH = "/" + CATEGORY_SAMPLE_TEMPLATES + "/consent/"
+)
+
+func InitAmpConsent() {
+	http.HandleFunc(CONSENT_SAMPLE_PATH+"getConsent", func(w http.ResponseWriter, r *http.Request) {
+		handlePost(w, r, submitConsentXHR)
+	})
+	http.HandleFunc(CONSENT_SAMPLE_PATH+"getVendors", submitVendorsXHR)
+}
+
+func submitConsentXHR(w http.ResponseWriter, r *http.Request) {
+	EnableCors(w, r)
+	SetContentTypeJson(w)
+	w.Write([]byte("{\"consentRequired\": true}"))
+}
+
+func submitVendorsXHR(w http.ResponseWriter, r *http.Request) {
+	EnableCors(w, r)
+	SetContentTypeJson(w)
+	w.Write([]byte("{\"items\": [{\"name\": \"Vendor1\"}, {\"name\": \"Vendor2\"}, {\"name\": \"Vendor3\"}]}"))
+}
