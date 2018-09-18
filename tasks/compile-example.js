bff7a4cb27817f5a933cbe8dbc73ab3f9c343781
Ben Morss <morss@google.com>
2018-09-18 02:41:39 -0400

    Add amp-consent (for GDPR) and ClientID (for analytics) (#1445)
    * Fixed minor typo, added comma
    
    * Added HTML and CSS for GDPR.
    
    * Made close and OK buttons actually work!
    
    * Added inclusion variables for <amp-consent> and <amp-geo>
    
    * Use new footer-boilerplate.html. Take consent.css back out of shared.css as it's not being included.
    More changes as per discussion with sbenz.
    
    * Include GDPR consent only if <amp-geo> and <amp-consent> aren't already on the page, as they are in consent/geo samples.
    If either of these components is used twice, AMP gets mad.
    
    * It's ok to include GDPR consent on Story pages.
    
    * Let's just pretend that last commit never existed, shall we?
    
    * Removed unnecessary extra classes from amp-consent
    
    * Add skipValidation to pages that will need to skip validation... for now...
    
    * Remove <amp-consent> from list of tags to auto-remove.
    
    * Don't insert <amp-consent> into stories.  Later, we'll make a mechanism to use <amp-consent-story> instead. (It needs to be a child of <amp-story>, so we can't insert it here.
    
    * New template for <amp-story-consent>.
    
    * Removed spacing as per @sebastianbenz
    
    * Re-enabled validation, as a test
    
    * Removed US
    
    * Added skipValidation back in.

--- tasks/compile-example.js
+++ tasks/compile-example.js
@@ -32,7 +32,7 @@ const storyBookend = require('./bookend.json');
 
 const STORY_EMBED_DIR = __dirname + '/../api/dist/';
 const AMP_STORY_CLEANER_REGEX =
-  ['amp-story', 'amp-story-auto-ads', 'amp-consent']
+  ['amp-story', 'amp-story-auto-ads']
       .map(extension =>
         new RegExp('<script\\s+async\\s+custom-element="' + extension
       + '"\\s+src="https:\\/\\/cdn\\.ampproject\\.org\\/v0\\/' + extension
@@ -229,6 +229,8 @@ module.exports = function(config, indexPath, updateTimestamp) {
         includesIframe: document.importsComponent('amp-iframe'),
         includesSelector: document.importsComponent('amp-selector'),
         includesSidebar: document.importsComponent('amp-sidebar'),
+        includesConsent: document.importsComponent('amp-consent'),
+        includesGeo: document.importsComponent('amp-geo'),
         includesServiceWorker:
         document.importsComponent('amp-install-serviceworker')
         || document.metadata.skipServiceWorker,
